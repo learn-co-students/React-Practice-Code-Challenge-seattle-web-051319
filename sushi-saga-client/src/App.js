5057922a9b9eab9008c57090e7218d117bb3e7c2
AustinBH <harlowaustin2@gmail.com>
2019-07-19 10:11:27 -0700

    meeting all initial deliverables

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
@@ -7,11 +7,62 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  constructor(){
+    super()
+    this.state = {
+      sushi: [],
+      displayedSushi: [],
+      plates: [],
+      moneyRemaining: 100,
+      isLoading: true
+    }
+  }
+
+  getSushi = () => {
+    fetch(API)
+    .then(res => res.json())
+    .then(json => {
+      this.setState({sushi: json, isLoading: false, displayedSushi: json.slice(0, 4)})
+    })
+
+  }
+
+  componentDidMount() {
+    this.getSushi()
+  }
+
+  eatSushi = oneSushi => {
+      const sushi = [...this.state.sushi].filter(singleSushi => singleSushi !== oneSushi)
+      const moneyRemaining = this.state.moneyRemaining - oneSushi.price
+      const plates = [...this.state.plates]
+      plates.push(oneSushi.name)
+      this.setState({ sushi, moneyRemaining, plates })
+  }
+
+  getMoreSushi = () => {
+    const shuffle = [...this.state.sushi].sort(() => 0.5 - Math.random())
+    const displayedSushi = shuffle.slice(0, 4)
+    this.setState({displayedSushi})
+  }
+
+  whileLoading = () => {
+    if (this.state.isLoading === true) {
+      return <p>Loading...</p>
+    }
+    else {
+      return (
+        <Fragment>
+        <SushiContainer sushi={this.state.displayedSushi} eatSushi={this.eatSushi} getMoreSushi={this.getMoreSushi} moneyRemaining={this.state.moneyRemaining}/>
+        <Table totalCash={this.state.moneyRemaining} plates={this.state.plates} />
+        </Fragment>
+      )
+    }
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        {this.whileLoading()}
       </div>
     );
   }
